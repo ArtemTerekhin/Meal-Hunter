f5ce27129779c99aaa770bb9fef89e9cedeb497b
Artem Terekhin <artem@byallmeans.work>
2025-07-06 22:03:25 +0200

    Integrate and configure SwiftLint

---
+++ MealHunter/MealHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealHunterApp.swift
+//  MealHunter
+//
+//  Created by Artem Terekhin on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MealHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
