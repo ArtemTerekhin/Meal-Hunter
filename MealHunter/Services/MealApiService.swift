b173ccc7c1df4c8433dad48f31a8bad2d15c169f
Artem Terekhin <artem@byallmeans.work>
2025-07-10 21:47:41 +0200

    Refactor: simplify MealAPIService using shared APIService and APIEndpoint

--- MealHunter/Services/MealApiService.swift
+++ MealHunter/Services/MealApiService.swift
@@ -13,20 +13,9 @@ protocol MealAPIServiceProtocol {
 
 final class MealAPIService: MealAPIServiceProtocol {
     func fetchMealDetail(id: String) async throws -> MealDetail {
-        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(id)") else {
-            throw URLError(.badURL)
-        }
+        let response: MealDetailResponse = try await APIService.shared.request(.mealDetail(id))
 
-        let (data, response) = try await URLSession.shared.data(from: url)
-
-        guard let httpResponse = response as? HTTPURLResponse,
-              (200...299).contains(httpResponse.statusCode) else {
-            throw APIError.invalidResponse
-        }
-
-        let result = try JSONDecoder().decode(MealDetailResponse.self, from: data)
-
-        guard let apiMeal = result.meals.first else {
+        guard let apiMeal = response.meals.first else {
             throw APIError.mealNotFound
         }
 
